1029a8615131dd2d65314ad3d845399f4bfa9353
Megan B <brown.m.j@outlook.com>
2017-07-27 19:35:51 -0500

    configure to support 4 routes

--- app/app.js
+++ app/app.js
@@ -3,6 +3,23 @@
 const notesApp = angular.module("NotesApp", ["ngRoute"]);
 
 notesApp.config( ($routeProvider) => {
-
+	$routeProvider
+	.when("/register", {
+		templateUrl: "partials/registration.html",
+		controller: "RegisterCtrl"
+	})
+	.when("/login", {
+		templateUrl: "partials/login.html",
+		controller: "LoginCtrl"
+	})
+	.when("/notes", {
+		templateUrl: "partials/note-list.html",
+		controller: "AllNotesCtrl"
+	})
+	.when("/new", {
+		templateUrl: "partials/new-note.html",
+		controller: "NewNoteCtrl"
+	})
+	.otherwise("/");
 });
 
